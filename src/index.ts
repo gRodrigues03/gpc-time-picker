61d8da1b5e9f89bda8d212bb5dbec05fc072e446
rodrigues.cco <bielcreations123@gmail.com>
2026-02-03 13:11:13 -0300

    Evitar renderização múltipla

--- src/index.ts
+++ src/index.ts
@@ -4,41 +4,44 @@ import {
 
 export default function onRender(args: FrontendRendererArgs): void {
     const { data, parentElement, setStateValue } = args;
+    let input = parentElement.querySelector(".cc-input") as HTMLInputElement;
 
-    const label = document.createElement("p");
-    label.className = 'cc-label'
-    label.textContent = data.label;
-    parentElement.appendChild(label);
+    if (!input) {
+        const label = document.createElement("p");
+        label.className = 'cc-label'
+        label.textContent = data.label;
+        parentElement.appendChild(label);
 
-    const input = document.createElement("input")
-    input.className = 'cc-input'
-    input.value = data.default;
-    parentElement.appendChild(input)
+        input = document.createElement("input")
+        input.className = 'cc-input'
+        input.value = data.default;
+        parentElement.appendChild(input)
 
-    input.addEventListener("change", function () {
-        setStateValue('t', input.value);
-    });
+        input.addEventListener("change", function () {
+            setStateValue('t', input.value);
+        });
 
-    input.addEventListener("input", (e) => {
-        let t = e.currentTarget,
-          n = t.selectionStart,
-          a = t.value.replace(/\D/g, ""),
-          i = a;
-        a.length >= 3 && (i = a.slice(0, 2) + ":" + a.slice(2, 4)),
-        i.length > t.value.length && n > 2 && n++,
-        i.length < t.value.length && n > 2 && n--,
-          (t.value = i.slice(0, 5)),
-          t.setSelectionRange(n, n);
-    });
+        input.addEventListener("input", (e) => {
+            let t = e.currentTarget as HTMLInputElement,
+              n = t.selectionStart,
+              a = t.value.replace(/\D/g, ""),
+              i = a;
+            a.length >= 3 && (i = a.slice(0, 2) + ":" + a.slice(2, 4)),
+            i.length > t.value.length && n > 2 && n++,
+            i.length < t.value.length && n > 2 && n--,
+              (t.value = i.slice(0, 5)),
+              t.setSelectionRange(n, n);
+        });
 
-    input.addEventListener("blur", (e) => {
-        let [t, n] = e.currentTarget.value.split(":");
-        t &&
-        n &&
-        ((t = Math.min(parseInt(t, 10), 23).toString().padStart(2, "0")),
-          (n = Math.min(parseInt(n, 10), 59).toString().padStart(2, "0")),
-          (e.currentTarget.value = `${t}:${n}`));
-    });
+        input.addEventListener("blur", (e) => {
+            let [t, n] = (e.currentTarget as HTMLInputElement).value.split(":");
+            t &&
+            n &&
+            ((t = Math.min(parseInt(t, 10), 23).toString().padStart(2, "0")),
+              (n = Math.min(parseInt(n, 10), 59).toString().padStart(2, "0")),
+              ((e.currentTarget as HTMLInputElement).value = `${t}:${n}`));
+        });
 
-    input.addEventListener("paste", e => e.preventDefault());
+        input.addEventListener("paste", e => e.preventDefault());
+    }
 }
